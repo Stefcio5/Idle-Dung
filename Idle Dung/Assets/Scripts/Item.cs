9529a7256d8a3318787f3963ed94fd4601427e4f
Stefcio5 <tefcio5@gmail.com>
2020-08-07 19:27:08 +0200

    Added CharacterStat, StatModifier, Character and Item scripts

---
+++ Idle Dung/Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public void Equip(Character c)
+    {
+        c.Strength.AddModifier(new StatModifier(.1f, StatModType.PercentMulti, this));
+        Debug.Log(c.Strength.Value);
+    }
+}
